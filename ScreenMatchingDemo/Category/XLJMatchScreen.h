3a87a1b577326b26bf591da377c611122a92d2e6
m <x@xlj.local>
2017-03-24 15:10:13 +0800

    添加服务器返回为空的处理

--- ScreenMatchingDemo/Category/XLJMatchScreen.h
+++ ScreenMatchingDemo/Category/XLJMatchScreen.h
@@ -175,6 +175,24 @@ XLJ_CGSizeMake(CGFloat Width, CGFloat Height)
     
 }
 
+//自定义字体
+//#define XLJ_FontSize(fontSize) [UIFont fontWithName:@"HiraginoSans-W3" size:fontSize]
+
+//字符串是否为空
+#define kStringIsEmpty(str) ([str isKindOfClass:[NSNull class]] || str == nil || [str length] < 1 ? YES : NO )
+
+//数组是否为空
+#define kArrayIsEmpty(array) (array == nil || [array isKindOfClass:[NSNull class]] || array.count == 0)
+
+//字典是否为空
+#define kDictIsEmpty(dic) (dic.count == 0 || [dic isKindOfClass:[NSNull class]] || dic.allKeys == 0)
+
+//是否是空对象
+#define kObjectIsEmpty(_object) (_object == nil \
+|| [_object isKindOfClass:[NSNull class]] \
+|| ([_object respondsToSelector:@selector(length)] && [(NSData *)_object length] == 0) \
+|| ([_object respondsToSelector:@selector(count)] && [(NSArray *)_object count] == 0))
+
 
 
 #endif /* XLJMatchScreen_h */
